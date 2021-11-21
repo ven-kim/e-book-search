afa9c9b35f8d7474ee99c09a5391c41fda87662f
Ven Kim <venvex@gmail.com>
2021-11-20 23:03:06 -0500

    attempt in fixing some errors with npm packages

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const {typeDefs, resolvers } = require('./schemas');
 const path = require('path');
+const {typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 const routes = require('./routes');
 
 const app = express();
@@ -30,6 +30,6 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL @ http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
